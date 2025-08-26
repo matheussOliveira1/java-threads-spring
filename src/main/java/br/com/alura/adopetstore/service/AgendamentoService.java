6ecea102e730719dc906959e40f9e427ce79ac91
matheussOliveira1 <matheus.aoliveira@sptech.school>
2025-08-26 12:45:15 -0300

    Projeto finalizado do estudo de threads com spring

---
+++ src/main/java/br/com/alura/adopetstore/service/AgendamentoService.java
@@ -0,0 +1,34 @@
+package br.com.alura.adopetstore.service;
+
+import br.com.alura.adopetstore.email.EmailRelatorioGerado;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class AgendamentoService {
+
+    @Autowired
+    private RelatorioService relatorioService;
+
+    @Autowired
+    private EmailRelatorioGerado sender;
+
+    @Scheduled(cron = "0 24 12 * * *")
+    public void envioEmailsAgendado() {
+        var estoqueZerado = relatorioService.infoEstoque();
+        var faturamentoObtido = relatorioService.faturamentoObtido();
+
+        CompletableFuture.allOf(estoqueZerado, faturamentoObtido).join();
+
+        try {
+            sender.enviar(estoqueZerado.get(), faturamentoObtido.get());
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
